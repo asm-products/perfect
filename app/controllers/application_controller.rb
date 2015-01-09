e2a0d4813c6b8ac0563d52c0abb46fb810cce25a
Zach Feldman <zachfeldman@gmail.com>
2015-01-09 01:02:51 -0500

    Remove SSL cert.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :expire_hsts
+
+  private
+  
+  def expire_hsts
+    response.headers["Strict-Transport-Security"] = 'max-age=0'
+  end
+
+
 end
